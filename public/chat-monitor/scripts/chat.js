ed6f64d578731f858f4cad42e178c2008e2b8bc5
LeinneSW <nno88551@naver.com>
2025-05-07 11:11:42 +0900

    chat-monitor : 채팅 전송 기능 추가

--- public/chat-monitor/scripts/chat.js
+++ public/chat-monitor/scripts/chat.js
@@ -2,6 +2,31 @@ let client;
 
 const getRequestUrl = () => window.localStorage.getItem('wsURL') || location.host
 
+const sendChat = async (event) => {
+    if(client?.readyState !== WebSocket.OPEN){
+        showToast('치지직 도우미가 종료된것 같습니다. 치지직 도우미를 켠 후 다시 시도해주세요.')
+        return
+    }
+
+    let input = event instanceof KeyboardEvent ? event.target : null
+    if(input && event.key !== 'Enter'){
+        return;
+    }else if(!input){
+        input = document.getElementById(`message-input`)
+    }
+
+    if(!input.value){
+        return;
+    }
+
+    client.send(JSON.stringify({
+        type: `SEND_MESSAGE`,
+        message: input.value
+    }))
+    input.value = ''
+    input.focus()
+}
+
 const connect = () => {
     if(client?.readyState === WebSocket.OPEN){
         return;
@@ -33,16 +58,30 @@ const connect = () => {
     client.onclose = () => setTimeout(() => connect(), 1000)
 }
 
-window.addEventListener('load', async () => {
-    // 채널명, 프사 취득하기
-    const user = await (await fetch('/user-info')).json();
+// 채널명, 프사 취득하기
+const updateSteamerInfo = async () => {
+    let user
+    while(!user){
+        user = await (await fetch('/user-info')).json();
+        if(!user){
+            await new Promise(resolve => setTimeout(resolve, 1000));
+        }
+    }
+
     const nickname = document.getElementById('streamer-name');
     const avatar = document.getElementById('streamer-avatar');
     const defaultURL = avatar.src;
     avatar.src = user.profileImageUrl || defaultURL;
-    avatar.onerror = () => {
-        avatar.src = defaultURL;
-    };
+    avatar.onerror = () => avatar.src = defaultURL;
     nickname.textContent = user.nickname;
+}
+
+window.addEventListener('load', () => {
+    const messageInput = document.getElementById('message-input');
+    messageInput.addEventListener('keydown', sendChat)
+    const sendBtn = document.getElementById('send-button')
+    sendBtn.addEventListener('click', sendChat)
+
+    updateSteamerInfo().catch(console.error);
     connect();
 });
