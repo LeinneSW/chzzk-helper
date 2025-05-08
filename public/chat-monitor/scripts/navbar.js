c78c9faa4051f67d7cfb36e76baa2128c4253224
LeinneSW <nno88551@naver.com>
2025-05-08 09:55:21 +0900

    chat-monitor : navbar 내 버튼 토글기능 로직 추가 및 구조 개선

--- public/chat-monitor/scripts/navbar.js
+++ public/chat-monitor/scripts/navbar.js
@@ -1,28 +1,32 @@
-window.addEventListener("load", () => {
-    // TTS 활성화 전환 버튼
-    const updateButton = () => {
-        if(localStorage.getItem('enableTTS') === '0'){ // TTS 비활성화
-            document.querySelector('.bi.bi-megaphone').style.display = ''
-            document.querySelector('.bi.bi-megaphone-fill').style.display = 'none'
-        }else{ // TTS 활성화
-            document.querySelector('.bi.bi-megaphone').style.display = 'none'
-            document.querySelector('.bi.bi-megaphone-fill').style.display = ''
-        }
+const toggleState = (button) => {
+    for(const child of button.children){
+        child.classList.toggle('hide');
     }
+}
 
-    updateButton()
+window.addEventListener("load", () => {
+    // 토글 버튼 초기화
+    const buttonList = document.querySelectorAll('.button.toggle');
+    buttonList.forEach(button => {
+        const saveName = button.dataset.saveName
+        if(saveName && localStorage.getItem(saveName) !== '0'){
+            toggleState(button)
+        }
+        button.addEventListener('click', () => toggleState(button))
+    })
+
+    // TTS 활성화 전환 버튼
     const ttsButton = document.getElementById('tts-button');
-    ttsButton.onclick = () => {
+    ttsButton.addEventListener('click', () => {
         const enabled = localStorage.getItem('enableTTS') || '1'
         localStorage.setItem('enableTTS', (+enabled + 1) % 2 + '')
         showToast(enabled === '1' ? 'TTS 기능이 비활성화 되었습니다.' : 'TTS 기능이 활성화 되었습니다')
-        updateButton()
-    }
+    })
 
     // 공지 등록 기능
     const chatContainer = document.getElementById('chat-container');
     const noticeButton = document.getElementById('notice-button');
-    noticeButton.onclick = () => {
+    noticeButton.addEventListener('click', () => {
         chatContainer.classList.toggle('select-notice');
         if(chatContainer.classList.contains('select-notice')){
             showToast('고정을 원하는 메시지를 선택해주세요');
@@ -30,8 +34,9 @@ window.addEventListener("load", () => {
                 const messageBox = e.target.closest('.message-box'); // 가장 가까운 .message-box 탐색
                 if(!messageBox) return;
 
-                chatContainer.onclick = () => {};
-                chatContainer.classList.remove('select-notice');
+                toggleState(noticeButton)
+                chatContainer.onclick = () => {}
+                chatContainer.classList.remove('select-notice')
                 fetch('/notice', {
                     method: "POST",
                     headers: {
@@ -53,9 +58,9 @@ window.addEventListener("load", () => {
                     .catch(() => showToast('공지 등록 실패! 권한이 없습니다.'))
             };
         }else{
-            chatContainer.onclick = () => {};
+            chatContainer.onclick = () => {}
         }
-    }
+    })
 
     // 설정 모달 버튼
     const modal = document.getElementById('settings-modal');
