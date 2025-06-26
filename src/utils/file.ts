e8409acb2121ffee14f17a8a83574b5ddfe3cab7
LeinneSW <nno88551@naver.com>
2025-06-26 09:37:18 +0900

    APP_ICON_PATH가 올바르게 동작하지 않던 버그 수정

--- src/utils/file.ts
+++ src/utils/file.ts
@@ -3,7 +3,7 @@ import {app} from "electron";
 import {mkdir, readFile, writeFile} from "fs/promises";
 import fsExists from "fs.promises.exists";
 
-export const APP_ICON_PATH = (() => path.join(__dirname, '../resources/icon.png'))();
+export const APP_ICON_PATH = (() => path.join(app.getAppPath(), 'resources/icon.png'))();
 
 export const getResourcePath = (fileOrDir: string = ''): string => {
     return path.join(app.getPath('userData'), 'resources', fileOrDir)
